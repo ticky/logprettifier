9b06d4eab09ae8ff409189bf2055d90854ebab7e
Geoff Stokes <geoff@geoffstokes.net>
2012-05-28 23:44:26 +1000

    Still takes SVN logs and turns them into markdown, but can now output as JSON too. Need to make it take arguments and turn that into changes to how it processes.

--- logprettifier.py
+++ logprettifier.py
@@ -1,50 +1,58 @@
 import dateutil.parser
+import json
 import pytz
 import sys
 from xml.dom import minidom
 
-def markdown_log(file_path):
+# TODO: Make this return a nicely-formatted string, rather than print directly.
+def print_markdown(logData):
+	for entry in logData:
+		print ('\n### r*%(revision)s*: **%(logMessage)s**' % {"revision": entry["changeset"], "logMessage": entry["message"]}).encode('utf-8')
+		print ('Submitted by %(author)s on %(date)s.\n' % {"author": entry["author"], "date": entry["date"]}).encode('utf-8')
+		for change in entry["files"]:
+			fileActions = {
+					"A":	"Added",
+					"D":	"Deleted",
+					"M":	"Modified",
+					"R":	"Replaced"
+			}
+			print ('* %(action)s `%(file)s`' % {"action": fileActions.get(change["action"]), "file": change["file"]}).encode('utf-8')
+
+def print_json(logData):
+	return json.dumps(logData)
+
+def parse_svn_xml(file_path):
 
 	dom = minidom.parse(file_path)
+
+	svnLogObject = []
 
 	for entry in dom.getElementsByTagName('logentry'):
 
 		if(entry.getElementsByTagName('msg').item(0).firstChild):
 
-			loginfo = {
-				"revision":		entry.getAttribute('revision'),
-				"logMessage":	entry.getElementsByTagName('msg').item(0).firstChild.nodeValue
-			}
-
 			tmpdate = dateutil.parser.parse(entry.getElementsByTagName('date').item(0).firstChild.nodeValue)
 
-			subinfo = {
-				"author":	entry.getElementsByTagName('author').item(0).firstChild.nodeValue,
-				"date":		tmpdate.astimezone(pytz.timezone('Australia/Melbourne')).strftime("%A, %d %B %Y at %H:%M:%S (%Z)")
-			}
-
-			print ('\n### r*%(revision)s*: **%(logMessage)s**' % loginfo).encode('utf-8')
-			print ('Submitted by %(author)s on %(date)s.\n' % subinfo).encode('utf-8')
+			svnLogObject.append({
+				"changeset":	entry.getAttribute('revision'),
+				"message":		entry.getElementsByTagName('msg').item(0).firstChild.nodeValue,
+				"author":		entry.getElementsByTagName('author').item(0).firstChild.nodeValue,
+				"date":			tmpdate.astimezone(pytz.timezone('Australia/Melbourne')).strftime("%A, %d %B %Y at %H:%M:%S (%Z)"),
+				"files":		[]
+			})
 
 			for fileNode in entry.getElementsByTagName('path'):
 
-				fileActions = {
-					"A":	"Added",
-					"D":	"Deleted",
-					"M":	"Modified",
-					"R":	"Replaced"
-				}
+				svnLogObject[-1]["files"].append({
+					"action":	fileNode.getAttribute('action'),
+					"file":		fileNode.firstChild.nodeValue
+				})
 
-				fileinfo = {
-					"action": fileActions.get(fileNode.getAttribute('action')),
-					"file": fileNode.firstChild.nodeValue
-				}
-
-				print ('* %(action)s `%(file)s`' % fileinfo).encode('utf-8')
+	return svnLogObject
 
 if(len(sys.argv) > 1):
-	# TODO: handle this more elegantly.
-	markdown_log(sys.argv[1])
+	# TODO: handle this (And switching modes!) more elegantly.
+	print(print_markdown(parse_svn_xml(sys.argv[1])))
 else:
 	print('# Error')
 	print('No arguments passed! Please give me a log file to parse.')
