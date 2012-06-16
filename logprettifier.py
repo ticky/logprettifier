80da2c274a0ec1855aa83b1ac87c8a04b006753b
Geoff Stokes <geoff@geoffstokes.net>
2012-06-16 18:37:03 +1000

    Tabs -> Spaces. Now does CGI.

--- logprettifier.py
+++ logprettifier.py
@@ -12,102 +12,100 @@ from xml.dom import minidom
 
 def print_markdown(logData):
 
-	markdownLog = []
+    markdownLog = []
 
-	for entry in logData:
+    for entry in logData:
 
-		# TODO: Do a better job of dealing and newlines (split then iterate?)
-		tmpdate = dateutil.parser.parse(entry["date"])
+        # TODO: Do a better job of dealing with newlines (split then iterate?)
+        tmpdate = dateutil.parser.parse(entry["date"])
 
-		markdownLog.append(
-			'### r*%(revision)s*: **%(logMessage)s**' %
-			{
-				"revision":		markdown_escape(entry["changeset"]),
-				"logMessage":	markdown_escape(entry["message"])
-			}
-		)
+        markdownLog.append(
+            '### r*%(revision)s*: **%(logMessage)s**' %
+            {
+                "revision":     markdown_escape(entry["changeset"]),
+                "logMessage":   markdown_escape(entry["message"])
+            }
+        )
 
-		numFiles = len(entry["files"])
-		if(numFiles == 1):
-			fileStr = "%i file" % numFiles
-		else:
-			fileStr = "%i files" % numFiles
+        numFiles = len(entry["files"])
+        if(numFiles == 1):
+            fileStr = "%i file" % numFiles
+        else:
+            fileStr = "%i files" % numFiles
 
-		markdownLog.append(
-			'**%(author)s** modified **%(files)s** on **%(date)s**' %
-			{
-				"author":	markdown_escape(entry["author"]),
-				"files":	markdown_escape(fileStr),
-				"date":		markdown_escape(tmpdate.astimezone(pytz.timezone('Australia/Melbourne')).strftime("%A, %d %B %Y at %H:%M:%S (%Z)"))
-			}
-		)
+        markdownLog.append(
+            '**%(author)s** modified **%(files)s** on **%(date)s**' %
+            {
+                "author":   markdown_escape(entry["author"]),
+                "files":    markdown_escape(fileStr),
+                "date":     markdown_escape(tmpdate.astimezone(pytz.timezone('Australia/Melbourne')).strftime("%A, %d %B %Y at %H:%M:%S (%Z)"))
+            }
+        )
 
-		markdownLog.append('')
+        markdownLog.append('')
 
-		for change in entry["files"]:
+        for change in entry["files"]:
 
-			fileActions = {
-					"A":	"Added",
-					"D":	"Deleted",
-					"M":	"Modified",
-					"R":	"Replaced"
-			}
+            fileActions = {
+                    "A":    "Added",
+                    "D":    "Deleted",
+                    "M":    "Modified",
+                    "R":    "Replaced"
+            }
 
-			markdownLog.append(
-				'* %(action)s `%(file)s`' %
-				{
-					"action":	fileActions.get(change["action"]),
-					"file":		change["file"]
-				}
-			)
+            markdownLog.append(
+                '* %(action)s `%(file)s`' %
+                {
+                    "action":   fileActions.get(change["action"]),
+                    "file":     change["file"]
+                }
+            )
 
-		markdownLog.append('\n') # Put two newlines between this and the next entry.
+        markdownLog.append('\n') # Put two newlines between this and the next entry.
 
-	return '\n'.join(markdownLog).encode('utf-8')
+    return '\n'.join(markdownLog).encode('utf-8')
 
 # TODO: Make this usable as an option
-def print_json(logData):
+def get_json(logData):
 
-	return json.dumps(logData)
+    return json.dumps(logData)
 
 def markdown_escape(inputString):
 
-	return inputString.replace("\\", "\\\\").replace("`","\\`").replace("*", "\\*").replace("_", "\\_").replace("-", "\\-").replace("+", "\\+").replace(".", "\\.").replace("!", "\\!")
+    return inputString.replace("\\", "\\\\").replace("`","\\`").replace("*", "\\*").replace("_", "\\_").replace("-", "\\-").replace("+", "\\+").replace(".", "\\.").replace("!", "\\!")
 
 # TODO: Handle missing/malformed string portions in the parse function
 def parse_svn_xml(file_path):
 
-	dom = minidom.parse(file_path)
+    dom = minidom.parse(file_path)
 
-	svnLogObject = []
+    svnLogObject = []
 
-	for entry in dom.getElementsByTagName('logentry'):
+    for entry in dom.getElementsByTagName('logentry'):
 
-		if(entry.getElementsByTagName('msg').item(0).firstChild):
+        if(entry.getElementsByTagName('msg').item(0).firstChild):
 
-			svnLogObject.append({
-				"changeset":	entry.getAttribute('revision'),
-				"message":		entry.getElementsByTagName('msg').item(0).firstChild.nodeValue,
-				"author":		entry.getElementsByTagName('author').item(0).firstChild.nodeValue,
-				"date":			entry.getElementsByTagName('date').item(0).firstChild.nodeValue,
-				"files":		[]
-			})
+            svnLogObject.append({
+                "changeset":    entry.getAttribute('revision'),
+                "message":      entry.getElementsByTagName('msg').item(0).firstChild.nodeValue,
+                "author":       entry.getElementsByTagName('author').item(0).firstChild.nodeValue,
+                "date":         entry.getElementsByTagName('date').item(0).firstChild.nodeValue,
+                "files":        []
+            })
 
-			for fileNode in entry.getElementsByTagName('path'):
+            for fileNode in entry.getElementsByTagName('path'):
 
-				svnLogObject[-1]["files"].append({
-					"action":	fileNode.getAttribute('action'),
-					"file":		fileNode.firstChild.nodeValue
-				})
+                svnLogObject[-1]["files"].append({
+                    "action":   fileNode.getAttribute('action'),
+                    "file":     fileNode.firstChild.nodeValue
+                })
 
-	return svnLogObject
+    return svnLogObject
 
-# TODO: Make this run happily in a CGI environment
-if(len(sys.argv) > 1):
-	# TODO: handle this (And switching modes!) more elegantly
-	#		http://docs.python.org/library/argparse.html
-	print(print_markdown(parse_svn_xml(sys.argv[1])))
-else:
-	# TODO: Print help?
-	print('# Error')
-	print('No arguments passed! Please give me a log file to parse.')
+# CGI: Header(s)
+print "X-Powered-By: Python"
+# print "Content-Type: text/json"
+# CGI: Blank Line to separate Headers and Content
+print ""
+# CGI: Output
+print(get_json(parse_svn_xml('C:\\log.xml')))
