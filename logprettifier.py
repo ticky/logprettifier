6938e16ced36afc4d28ce8292f815c629a7ead89
Geoff Stokes <geoff@geoffstokes.net>
2012-06-16 23:19:35 +1000

    Updated comments and TODOs, removed now unused functionality.

--- logprettifier.py
+++ logprettifier.py
@@ -1,89 +1,44 @@
 #!/usr/bin/env python
 
 # logprettifier.py
-# Takes source control logs and turns them into either Markdown (Default)
-# or JSON for further processing or use in web apps.
+# Retrieves multiple source control logs, combines them and converts to JSON for
+# further processing or use in web apps.
 
 # Parameters (Not yet implemented)
 # - repo: the repo name specified in the config file.
 #         (Note: when unspecified, all enabled repos are fetched.)
 
+# Timezone Utilities
 import dateutil.parser
-import json
-import pytz
+#import pytz
+
+# System IO Utilities
 import sys
 import subprocess
+
+# Serialisation Utilities
 from xml.dom import minidom
+import json
+
+# CGI Utilities
 import cgitb
 cgitb.enable()
 
-def print_markdown(logData):
-
-    markdownLog = []
-
-    for entry in logData:
-
-        # TODO: Do a better job of dealing with newlines (split then iterate?)
-        tmpdate = dateutil.parser.parse(entry["date"])
-
-        markdownLog.append(
-            '### r*%(revision)s*: **%(logMessage)s**' %
-            {
-                "revision":     markdown_escape(entry["changeset"]),
-                "logMessage":   markdown_escape(entry["message"])
-            }
-        )
-
-        numFiles = len(entry["files"])
-        if(numFiles == 1):
-            fileStr = "%i file" % numFiles
-        else:
-            fileStr = "%i files" % numFiles
-
-        markdownLog.append(
-            '**%(author)s** modified **%(files)s** on **%(date)s**' %
-            {
-                "author":   markdown_escape(entry["author"]),
-                "files":    markdown_escape(fileStr),
-                "date":     markdown_escape(tmpdate.astimezone(pytz.timezone('Australia/Melbourne')).strftime("%A, %d %B %Y at %H:%M:%S (%Z)"))
-            }
-        )
-
-        markdownLog.append('')
-
-        for change in entry["files"]:
-
-            fileActions = {
-                    "A":    "Added",
-                    "D":    "Deleted",
-                    "M":    "Modified",
-                    "R":    "Replaced"
-            }
-
-            markdownLog.append(
-                '* %(action)s `%(file)s`' %
-                {
-                    "action":   fileActions.get(change["action"]),
-                    "file":     change["file"]
-                }
-            )
-
-        markdownLog.append('\n') # Put two newlines between this and the next entry.
-
-    return '\n'.join(markdownLog).encode('utf-8')
-
-# TODO: Make this usable as an option
-def get_json(logData):
-
-    return json.dumps(logData)
-
-def markdown_escape(inputString):
-
-    return inputString.replace("\\", "\\\\").replace("`","\\`").replace("*", "\\*").replace("_", "\\_").replace("-", "\\-").replace("+", "\\+").replace(".", "\\.").replace("!", "\\!")
-
+# TODO: Turn this into a class with constructors and useful OO things so it can
+#       be re-used and expanded upon.
 def get_svn_data():
 
-    svnProcess = subprocess.Popen(['/usr/bin/svn', 'log', '--xml', '-l', '500', '-v', 'http://source.colloquy.info/svn/trunk'],stdout=subprocess.PIPE,stderr=subprocess.PIPE)
+    svnProcess = subprocess.Popen(
+        [
+            '/usr/bin/svn', 'log',
+            '--xml',
+            '-l', '500',
+            '-v',
+            'http://source.colloquy.info/svn/trunk'
+        ],
+        stdout=subprocess.PIPE,
+        stderr=subprocess.PIPE
+    )
     output, errors = svnProcess.communicate()
 
     dom = minidom.parseString(output)
@@ -111,12 +66,14 @@ def get_svn_data():
 
     return svnLogObject
 
+# TODO: Write a simple CGI wrapper allowing for a buffer to be written to before
+#       flushing as response.
 # CGI: Header(s)
 print "X-Powered-By: Python"
 # print "Content-Type: text/html"
 print "Content-Type: application/json"
 # CGI: Blank Line to separate Headers and Content
 print ""
+
 # CGI: Output
-svnData = get_svn_data()
-print(get_json(svnData))
+print(json.dumps(get_svn_data()))
